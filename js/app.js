34433374f6764d173ac86b0a40f01de12c781e57
Simon Philouze <contact@simonphilouze.fr>
2018-09-02 11:30:09 +0200

    add app js file

---
+++ js/app.js
@@ -0,0 +1,22 @@
+new Vue({
+  el: '#app',
+  data: {
+    citations: [],
+    activeCitation : {}
+  },
+  methods: {
+    random: function() {
+      let rangeMax = this.citations.length;
+      let rand = _.random(0, rangeMax - 1);
+      this.activeCitation = this.citations[rand];
+    }
+  },
+  mounted () {
+    $.getJSON('bin/citations.json', json => {
+      this.citations = json
+      let rangeMax = this.citations.length;
+      let rand = _.random(0, rangeMax - 1);
+      this.activeCitation = this.citations[rand];
+    })
+  }
+})
