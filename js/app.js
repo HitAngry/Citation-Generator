5d540cceb9657562ea678ef87d65618d8b605bd5
Simon Philouze <contact@simonphilouze.fr>
2019-02-09 15:40:43 +0100

    add capitilizeFirstLetter, PointToEnd, and fix alog gen

--- js/app.js
+++ js/app.js
@@ -2,19 +2,36 @@ new Vue({
   el: '#app',
   data: {
     citations: [],
-    activeCitation : {}
+    generatedCitation : {},
+    filter: 'actuelle'
   },
   methods: {
-    random: function() {
-      let rangeMax = this.citations.length;
-      let rand = _.random(0, rangeMax - 1);
-      this.activeCitation = this.citations[rand];
+    capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+    pointToEnd(string) {
+      if(string.charAt(string.length-1) !== '.') {
+        string = [...string, '.'];
+      } 
+      return string
+    },
+    generate: function() {
+      let citation = this.citations[_.random(0, this.citations.length - 1)];
+      let text = citation.text.split(' ');
+      text.splice(_.random(0, text.length), 0, this.filter);
+      text = text.join(' ');
+      text = this.capitalizeFirstLetter(text);
+      text = this.pointToEnd(text);
+      this.generatedCitation = {'text': text, 'author': citation.author, 'origin': citation.origin};
+    },
+    updateFilter: function(filter) {
+      this.filter = filter;
     }
   },
   mounted () {
     $.getJSON('bin/citations.json', json => {
-      this.citations = json
-      this.random();
+      this.citations = json;
+      this.generate();
     })
   }
 })
